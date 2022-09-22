e744cf56c580ac91f2744128e1bd658154b90bca
arpeggio <wongjl.chester@gmail.com>
2022-09-22 12:07:28 +0800

    NavBar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function NavBar () {
+    const pokedexLogo = 'https://falberthen.github.io/assets/img/posts/2022-02-12-pokedex/pokedex-logo.png'
+    return (
+        <nav>
+            <div>
+                <img alt ='pokedex-logo' src ={pokedexLogo} className="navbar-img" />
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
